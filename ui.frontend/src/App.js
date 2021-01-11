0612d46148ce2d919267896f6829783e8f3b8073
Daniel Gordon <dgordon@adobe.com>
2021-01-11 08:05:42 -0800

    updated to use GraphQL app

--- ui.frontend/src/App.js
+++ ui.frontend/src/App.js
@@ -1,16 +1,58 @@
-import { Page, withModel } from '@adobe/aem-react-editable-components';
-import React from 'react';
+/*
+Copyright 2020 Adobe
+All Rights Reserved.
 
-// This component is the application entry point
-class App extends Page {
-  render() {
-    return (
-      <div>
-        {this.childComponents}
-        {this.childPages}
+NOTICE: Adobe permits you to use, modify, and distribute this file in
+accordance with the terms of the Adobe license agreement accompanying
+it.
+*/
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import logo from './images/wknd-logo-dk.svg';
+import Adventures from './components/Adventures';
+import AdventureDetail from './components/AdventureDetail';
+import './App.scss';
+import { AEMText } from './components/AEMText';
+
+function App() {
+
+  return (
+    <Router>
+      <div className="App">
+        <header>
+          <img src={logo} className="logo" alt="WKND Logo"/>
+          <hr />
+        </header>
+      <Switch>
+        <Route path='/adventure:path'>
+          <AdventureDetail />
+        </Route>  
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
       </div>
-    );
-  }
+    </Router>   
+  );
 }
 
-export default withModel(App);
+/***
+ * Displays a grid of current adventures
+ */
+function Home() {
+  return (
+    <div className="Home">
+      <h2>Current Adventures</h2>
+      <AEMText
+        pagePath='/content/wknd-spa-react/us/en/home'
+        itemPath='/root/responsivegrid/text' />
+      <Adventures />
+      <AEMText
+        pagePath='/content/wknd-spa-react/us/en/home'
+        itemPath='/root/responsivegrid/text20' />
+  </div>
+  );
+}
+
+
+export default App;
